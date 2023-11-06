447bdff8a89acd575054218b4766730e18276a77
Rrinvil <144148135+Rrinvil@users.noreply.github.com>
2023-11-06 11:25:33 -0500

    Add files via upload

---
+++ lab_6-1.py
@@ -0,0 +1,17 @@
+# Creates a list containing the numbers 1 to 5 and stores it as a variable called my_arr.
+my_arr = [1, 2, 3, 4, 5]
+
+
+print(my_arr[2])
+# This prints the element at index 2, which is the number 3.
+
+print(len(my_arr))
+ # This prints the length of the list, which is 5
+
+my_arr_repeated = my_arr * 3 # This creates a new list that contains my_arr repeated 3 times.
+print(my_arr_repeated) # This prints the repeated list.
+
+
+string = "string"
+string_as_list = list(string) # This converts the string into a list of individual characters.
+print(string_as_list) # This prints the list of characters.
